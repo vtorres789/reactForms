39bb2c4188b814954efa1b47925d1ad5b2f89eff
vtorres789 <victortorres180@gmail.com>
2023-07-27 20:16:11 -0400

    workshop progress

---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import React from "react";
+
+
+
+export default function SignUpForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
+    
+    async function handleSubmit(event) {
+        event.preventDefault();
+        try {
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup");
+            const result = await response.json();
+            console.log(result);
+        } catch (error) {
+          setError(error.message);
+        }
+      }
+
+    return (
+    <>
+    <h2>Sign Up!</h2>
+    {error && <p>{error}</p>}
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username: {" "} <input value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                Password: {" "} <input value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <button>Submit</button>
+        </form>
+    </>
+    );
+    
+}
